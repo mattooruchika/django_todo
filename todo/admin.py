c8596b07d247e9dbc7d8387c60cb4a6864568873
mattooruchika <ruchikamattoo09@gmail.com>
2021-11-04 18:31:54 +0530

    added gitignore

--- todo/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from todo.models import *
-
-class ToDoAdmin(admin.ModelAdmin):
-    readonly_fields=('dt_created',)
-admin.site.register(ToDo,ToDoAdmin)
-
-# Register your models here.
